89a8dfa57b0f27cbf6756bab8a8430c5115ccbdc
is1jayet <1793216505@qq.com>
2025-12-01 10:49:54 +0800

    Initial commit: Spring Cloud电商平台项目

---
+++ src/main/java/edu/ecopm/system/EcopmSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.ecopm.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcopmSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ecopm/system/EcopmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ecopm.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcopmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
